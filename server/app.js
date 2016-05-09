7063367d5ac6ecea6ce595fca3835947dac96c9b
Charles Hansen <chansen@pivotal.io>
2016-05-09 10:16:03 -0700

    Do not override development assets with production assets in development mode

--- server/app.js
+++ server/app.js
@@ -7,12 +7,15 @@ const {useWebpackDevMiddleware} = config;
 module.exports = function() {
   const app = express();
 
-  app.use(express.static(path.join(__dirname, '..', 'public')));
 
   if (useWebpackDevMiddleware) {
     const webpackHotMiddleware = require('pui-react-tools/middleware/webpack');
     app.use(...webpackHotMiddleware());
+    app.use(express.static(path.join(__dirname, '..', 'public')));
     app.get('*', webpackHotMiddleware.url('/index.html'));
+  } else {
+    app.use(express.static(path.join(__dirname, '..', 'public')));
   }
+
   return app;
 };
