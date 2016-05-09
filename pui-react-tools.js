e74080b4f481566747041d20fe5ec7e0dcbaf7bd
Charles Hansen <chansen@pivotal.io>
2016-05-09 10:15:21 -0700

    Bump pui-react-tools to get allow local assets in scss

--- pui-react-tools.js
+++ pui-react-tools.js
@@ -1,10 +1,5 @@
 module.exports = {
   webpack: {
-    devtool: 'cheap-module-source-map',
-    entry: {
-      application: ['./app/components/application.js', 'webpack-hot-middleware/client']
-    },
     integration: { devtool: 'source-map' }
-
   }
 };
