594557326047566670983ea9a618198171e9b4a1
Charles Hansen <chansen@pivotal.io>
2016-05-12 08:27:39 -0700

    Add warning if global namespace is changed without changing references to it

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,3 +1,4 @@
+const invariant = require('invariant');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
@@ -6,6 +7,10 @@ module.exports = {
     if (typeof document === 'undefined') return;
     require('./stylesheets/application.scss');
     require('babel!pui-react-tools/assets/entry-loader?name=index.html!./components/application');
+    invariant(global.MyReactStarter,
+      `globalNamespace in application.json has been changed without updating global variable name bootstrap.js.
+      Please change "MyReactStarter" in bootstrap.js to your current globalNamespace`
+    );
     const {config} = global.MyReactStarter;
     ReactDOM.render(<Entry {...props} {...{config}}/>, root);
   }
