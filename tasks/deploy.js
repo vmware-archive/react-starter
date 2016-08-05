52fee79a950ee7c0ff2ceaa389e7110698480559
Charles Hansen <chansen@pivotal.io>
2016-08-05 12:24:06 -0700

    Enable pushstate on CF deployment [finishes #121787351]

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -6,10 +6,14 @@ gulp.task('push', (callback) => {
   spawn('cf', ['push'], {stdio: 'inherit', env: process.env}).once('close', callback);
 });
 
+gulp.task('copy-staticfile', () => {
+  return gulp.src('Staticfile').pipe(gulp.dest('public'));
+});
+
 gulp.task('deploy', (done) => {
   const {NODE_ENV: env} = process.env;
   process.env.NODE_ENV = 'production';
-  runSequence('assets', 'push', () => {
+  runSequence('assets', 'copy-staticfile', 'push', () => {
     process.env.NODE_ENV = env;
     done();
   });
