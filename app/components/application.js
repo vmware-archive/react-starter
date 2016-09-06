d2a4d5b7f64689f1175aedeab62a7e58c36e5f3e
Charles Hansen <chansen@pivotal.io>
2016-09-06 09:26:38 -0700

    first pass at flexbox instead of grids

--- app/components/application.js
+++ app/components/application.js
@@ -5,6 +5,8 @@ const types = React.PropTypes;
 const {useStore} = require('p-flux');
 const {useRouter} = require('./use_router');
 const Router = require('./router');
+require('pui-css-alignment');
+require('pui-css-whitespace');
 
 class Application extends React.Component {
   static propTypes = {
@@ -17,13 +19,6 @@ class Application extends React.Component {
     const {config, store, router} = this.props;
     return (
       <div className="pui-react-starter">
-        <a href="todoList" onClick={e => {e.preventDefault(); router.navigate('/todoList');}}>Todo List!</a>
-        <br/>
-        <a href="apiPage" onClick={e => {e.preventDefault(); router.navigate('/apiPage');}}>Page that hits an api</a>
-        <br/>
-        <a href="createNewUser" onClick={e => {e.preventDefault(); router.navigate('/users/new');}}>Create New User</a>
-        <br/>
-        <a href="userList" onClick={e => {e.preventDefault(); router.navigate('/users/list');}}> All Users</a>
         <Router {...{router, config, ...store}}/>
       </div>
     );
--- app/components/todo_page.js
+++ app/components/todo_page.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const types = require('react').PropTypes;
-const TodoAdder = require('./todo_adder');
-const TodoList = require('./todo_list');
+const {Input} = require('pui-react-inputs');
+const {DefaultButton} = require('pui-react-buttons');
 
 class TodoPage extends React.Component {
   static propTypes = {
@@ -10,15 +10,43 @@ class TodoPage extends React.Component {
   };
 
   render() {
-    const {config: {title}, todoItems} = this.props;
-    return (
-      <div className="todo-page">
-        <h3 className="title">{title}</h3>
-        <h3>Things to do</h3>
-        <TodoAdder/>
-        <TodoList todoItems={todoItems}/>
+    return (<div>
+      <h1>You might not need grids, this is all flexbox</h1>
+      <h2>Equal Widths: (flex: 1;)</h2>
+      <div className="flex-container">
+        <div className="grid-markings flex-1 phm">Contact Info:</div>
+        <div className="grid-markings flex-1 phm"><Input placeholder="Type email here"/></div>
+        <div className="grid-markings flex-1 phm"><Input placeholder="Phone Number"/></div>
+        <div className="grid-markings flex-1 phm"><DefaultButton>Submit</DefaultButton></div>
       </div>
-    );
+
+      <h2>Vertical Centering: (flex-direction: row; align-items: center;)</h2>
+      <div className="flex-row-center">
+        <div className="grid-markings phm">Contact Info:</div>
+        <div className="grid-markings phm"><Input className="mbn" placeholder="Type email here"/></div>
+        <div className="grid-markings phm"><Input className="mbn" placeholder="Phone Number"/></div>
+        <div className="grid-markings phm"><DefaultButton>Submit</DefaultButton></div>
+      </div>
+
+
+      <h2>Stuff on the right (flex: 1; text-align: right;)</h2>
+      <div className="flex-container">
+        <div className="grid-markings phm">Contact Info:</div>
+        <div className="grid-markings phm"><Input className="mbn" placeholder="Type email here"/></div>
+        <div className="grid-markings phm"><Input className="mbn" placeholder="Phone Number"/></div>
+        <div className="grid-markings flex-1 txt-r phm"><DefaultButton>Submit</DefaultButton></div>
+      </div>
+
+      <h2>Stuff on the right (justify-content: space-between;)</h2>
+      <div className="flex-space-between">
+        <div className="flex-container">
+          <div className="grid-markings phm">Contact Info:</div>
+          <div className="grid-markings phm"><Input className="mbn" placeholder="Type email here"/></div>
+          <div className="grid-markings phm"><Input className="mbn" placeholder="Phone Number"/></div>
+        </div>
+        <div className="grid-markings phm"><DefaultButton>Submit</DefaultButton></div>
+      </div>
+    </div>);
   }
 }
 
