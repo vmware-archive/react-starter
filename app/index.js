1e4de3f456446099b92cce2265e6995d8af40402
Ryan Dy <rdy@pivotal.io>
2016-12-19 09:41:29 -0800

    wip
    Signed-off-by: Jean de Klerk <jadekler@gmail.com>

---
+++ app/index.js
@@ -0,0 +1,29 @@
+const invariant = require('invariant');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Application = require('./components/application');
+
+require('babel!pui-react-tools/assets/entry-loader?name=index.html!./components/application');
+invariant(global.MyReactStarter,
+  `globalNamespace in application.json has been changed without updating global variable name bootstrap.js.
+  Please change "MyReactStarter" in bootstrap.js to your current globalNamespace`
+);
+
+const {config} = global.MyReactStarter;
+ReactDOM.render(
+  <AppContainer>
+    <Application {...{config}}/>
+  </AppContainer>, root
+);
+
+if (module.hot) {
+  module.hot.accept('./components/application', () => {
+    const NextApp = require('./components/application');
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp {...{config}} />
+      </AppContainer>,
+      rootEl
+    );
+  });
+}
