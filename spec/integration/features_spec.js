2b26eea29c541519b25872a526624fcd5787189b
Charles Hansen <chansen@pivotal.io>
2017-01-16 12:07:49 -0800

    use jasmine async suite instead of callback style for features spec
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- spec/integration/features_spec.js
+++ spec/integration/features_spec.js
@@ -4,17 +4,15 @@ describeWithWebdriver('Features', () => {
   let page;
 
   describe('when viewing the app', () => {
-    beforeEach(async (done) => {
+    beforeEach.async(async() => {
       page = (await visit('/')).page;
       await waitForExist(page, '.pui-react-starter');
-      done();
     });
 
-    it('can add a todoItem', async (done) => {
+    it.async('can add a todoItem', async() => {
       await setValue(page, '.todo-adder input', 'DO THIS THING');
       await click(page, '.todo-adder button');
       await waitForText(page, '.todo-list .todo-item', 'DO THIS THING');
-      done();
     });
   });
 });
--- spec/integration/spec_helper.js
+++ spec/integration/spec_helper.js
@@ -1,6 +1,9 @@
 require('../support/bluebird');
 require('../spec_helper');
 const webdriverHelper = require('./helpers/webdriver_helper');
+const JasmineAsyncSuite = require('jasmine-async-suite');
+
+JasmineAsyncSuite.install();
 
 const {DEFAULT_TIMEOUT_INTERVAL} = jasmine;
 
@@ -14,4 +17,5 @@ beforeAll(() => {
 afterAll(() => {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = DEFAULT_TIMEOUT_INTERVAL;
   Object.keys(globals).forEach(key => delete global[key]);
+  JasmineAsyncSuite.uninstall();
 });
