9244b200649bf42c233c735b42f84633843d1558
Ryan Dy <rdy@pivotal.io>
2017-01-16 15:42:51 -0800

    Use firefox on travis
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- spec/integration/support/jasmine_webdriver.js
+++ spec/integration/support/jasmine_webdriver.js
@@ -6,7 +6,7 @@ const webdriverio = require('webdriverio');
 const privates = new WeakMap();
 
 class JasmineWebdriver {
-  constructor({browser = 'phantomjs', timeout = 500} = {}) {
+  constructor({browser = process.env.BROWSER || 'phantomjs', timeout = 500} = {}) {
     privates.set(this, {processes: [], desiredCapabilities: {browserName: browser}, timeout});
   }
 
